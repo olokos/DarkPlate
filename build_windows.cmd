23f00e70259b0b3a3d18b385112b76fc158e9368
olokos <1755581+olokos@users.noreply.github.com>
2023-02-04 00:41:32 +0100

    CUDA_BIN_PATH Variable in Windows script
    Having another variable for this makes it more obvious it needs to be updated if CUDA has a different version or directory.

--- build_windows.cmd
+++ build_windows.cmd
@@ -15,15 +15,16 @@ set BUILD_TYPE=Release
 set VCPKG_PATH=C:\DarknetOpenCV\vcpkg
 rem set TRIPLET=x64-windows-static
 set TRIPLET=x64-windows
+set CUDA_BIN_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8/bin"
 
-cmake -A %ARCHITECTURE% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DCMAKE_CUDA_COMPILER="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8/bin" ..
+cmake -A %ARCHITECTURE% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DCMAKE_CUDA_COMPILER=%CUDA_BIN_PATH% ..
 if ERRORLEVEL 1 goto END
 
-msbuild.exe /property:Platform=%ARCHITECTURE%;Configuration=%BUILD_TYPE% /target:Build -maxCpuCount -verbosity:normal -detailedSummary DarkPlate.sln
+ msbuild.exe /property:Platform=%ARCHITECTURE%;Configuration=%BUILD_TYPE% /target:Build -maxCpuCount -verbosity:normal -detailedSummary DarkPlate.sln
 if ERRORLEVEL 1 goto END
 
-msbuild.exe /property:Platform=%ARCHITECTURE%;Configuration=%BUILD_TYPE% PACKAGE.vcxproj
-if ERRORLEVEL 1 goto END
+rem msbuild.exe /property:Platform=%ARCHITECTURE%;Configuration=%BUILD_TYPE% PACKAGE.vcxproj
+rem if ERRORLEVEL 1 goto END
 
 :END
 cd ..
