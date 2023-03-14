15e470f45c08d42d23896a1925890652e3c8c871
olokos <1755581+olokos@users.noreply.github.com>
2023-03-14 20:06:12 +0100

    Fill-in missing variables on windows with Cuda 11.8

--- build_windows_debug.cmd
+++ build_windows_debug.cmd
@@ -17,7 +17,7 @@ rem set TRIPLET=x64-windows-static
 set TRIPLET=x64-windows
 set CUDA_BIN_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8/bin"
 
-cmake -A %ARCHITECTURE% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DCMAKE_CUDA_COMPILER=%CUDA_BIN_PATH% ..
+cmake -A %ARCHITECTURE% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DCMAKE_CUDA_COMPILER=%CUDA_BIN_PATH% -DCUDA_SDK_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8" -DCUDNN_LIBRARY_DLL="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8/cudnn64_8.dll" ..
 if ERRORLEVEL 1 goto END
 
 msbuild.exe /property:Platform=%ARCHITECTURE%;Configuration=%BUILD_TYPE% /target:Build -maxCpuCount -verbosity:normal -detailedSummary DarkPlate.sln
